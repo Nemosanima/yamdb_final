eee06350977dbeab011f2ef2aa8ea398c8f86ce5
Rasul-code <sadlarkhanov-rasul@mail.ru>
2023-05-15 21:18:46 +0400

    added ip

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = "p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs"
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['130.193.42.229', 'localhost']
 
 # For model User
 AUTH_USER_MODEL = 'reviews.User'
